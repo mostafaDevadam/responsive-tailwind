b27ccc3c1e7cd4285274d026482730387bb72a43
DESKTOP-8S79U4C\mustafa farrag <mustafamofarrag@gmail.com>
2025-01-18 15:01:09 +0200

    create responsive

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  darkMode: 'class',
+  theme: {
+    screens: {
+      'sm': '300px', //'640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      colors: {
+        dim: {
+          50: "#5F99F7",
+          100: "#5F99F7",
+          200: "#38444d",
+          300: "#202e3a",
+          400: "#253341",
+          500: "#5F99F7",
+          600: "#5F99F7",
+          700: "#192734",
+          800: "#162d40",
+          900: "#15202b",
+        }
+      },
+      animation: {
+        "spin-fast": "spin 0.5s linear infinite",
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
